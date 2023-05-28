d08a31762770ba07221cb03b0b5c258e16c50270
Yerong <yerong.li@outlook.com>
2023-05-28 16:33:05 -0500

    Log the model type

--- trainers/kgmoe_trainer.py
+++ trainers/kgmoe_trainer.py
@@ -59,18 +59,14 @@ class KGMoESeq2SeqTrainer(Seq2SeqTrainer):
         self.loss_ratio = self.data_args.loss_ratio
 
     def _training_step(self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]], optimizer) -> torch.Tensor:
-        logging.info("inputs")
+        # logging.info("inputs")
         # logging.info(inputs.keys())
         # 2023-05-28 02:55:32 INFO     dict_keys(['input_ids', 'attention_mask', 'decoder_input_ids', 'labels', 'concept_ids', 'concept_distances', 'concept_labels', 'oracle_concept_ids', 'head_ids', 'tail_ids', 'relation_ids', 'triple_labels'])
         
         # logging.info(self.tokenizer)
         # <transformers.tokenization_bart.BartTokenizer object at 0x2acbab8e8490>
-        logging.info(self.tokenizer.decode(inputs['input_ids'][0], skip_special_tokens=True))
-        logging.info(self.tokenizer.decode(inputs['labels'][0],skip_special_tokens=True))
-        try:
-            logging.info(self.tokenizer.decode(inputs['concept_labels'][0],skip_special_tokens=True))
-        except:
-            logging.info(inputs['concept_labels'][0])
+        # logging.info(self.tokenizer.decode(inputs['input_ids'][0], skip_special_tokens=True))
+        # logging.info(self.tokenizer.decode(inputs['labels'][0],skip_special_tokens=True))
         # 2023-05-28 03:34:22 INFO - kgmoe_trainer.py - inputs
         # 2023-05-28 03:34:22 INFO - kgmoe_trainer.py -  All cats chirp. bird container box apartment mouth mouse sun ranch backyard device car live computer floor land dust apply house sofa nature rug bird container box apartment mouth mouse sun ranch backyard device car live computer floor land dust apply house sofa nature rug
         # 2023-05-28 03:34:22 INFO - kgmoe_trainer.py -  only birds chirp.
@@ -79,6 +75,28 @@ class KGMoESeq2SeqTrainer(Seq2SeqTrainer):
         # 2023-05-28 03:34:23 INFO - kgmoe_trainer.py -  tomato is very small, no one can be fit to take nap on it.
         # 2023-05-28 03:34:24 INFO - kgmoe_trainer.py - inputs
         # 2023-05-28 03:34:24 INFO - kgmoe_trainer.py -  Music is a tasting activity,. camp flat paint hop heal metal toy product literature harmony pitch ballet music lead pleasure language talk learning wave point camp flat paint hop heal metal toy product literature harmony pitch ballet music lead pleasure language talk learning wave point
+
+        # try:
+        #     logging.info(self.tokenizer.decode(inputs['concept_labels'][0],skip_special_tokens=True))
+        # except:
+        #     logging.info(inputs['concept_labels'][0])
+        # 2023-05-28 03:42:14 INFO - kgmoe_trainer.py - tensor([ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        #          0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        #          0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        #          0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        #          0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        #          0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        #          0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        #          0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        #          0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        #          0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        #          0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        #          0,  0,  0,  0,  0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        #         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        #         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        #         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        #         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        #         -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1])
         self.B, self.L = inputs['labels'].shape
         self.BC, self.LC = inputs['concept_labels'].shape
         assert self.B == self.BC
@@ -124,6 +142,8 @@ class KGMoESeq2SeqTrainer(Seq2SeqTrainer):
             
         # do the expert training!
         model.train()
+        logging.info('model')
+        logging.info(model)
         lm_loss, kg_loss = self.compute_loss(model, inputs)
         loss = lm_loss + self.loss_ratio * kg_loss
 
