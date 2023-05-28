4a86677bf600978674c79c0a6767a50b3c831add
Yerong <yerong.li@outlook.com>
2023-05-28 03:41:25 -0500

    Log concept_labels

--- trainers/kgmoe_trainer.py
+++ trainers/kgmoe_trainer.py
@@ -67,7 +67,10 @@ class KGMoESeq2SeqTrainer(Seq2SeqTrainer):
         # <transformers.tokenization_bart.BartTokenizer object at 0x2acbab8e8490>
         logging.info(self.tokenizer.decode(inputs['input_ids'][0], skip_special_tokens=True))
         logging.info(self.tokenizer.decode(inputs['labels'][0],skip_special_tokens=True))
-        logging.info(self.tokenizer.decode(inputs['concept_labels'][0],skip_special_tokens=True))
+        try:
+            logging.info(self.tokenizer.decode(inputs['concept_labels'][0],skip_special_tokens=True))
+        except
+            logging.info(inputs['concept_labels'][0])
         # 2023-05-28 03:34:22 INFO - kgmoe_trainer.py - inputs
         # 2023-05-28 03:34:22 INFO - kgmoe_trainer.py -  All cats chirp. bird container box apartment mouth mouse sun ranch backyard device car live computer floor land dust apply house sofa nature rug bird container box apartment mouth mouse sun ranch backyard device car live computer floor land dust apply house sofa nature rug
         # 2023-05-28 03:34:22 INFO - kgmoe_trainer.py -  only birds chirp.
