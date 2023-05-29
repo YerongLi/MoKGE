e3660d7d33d55253b6b7593edcb36f07852d7afb
Yerong <yerong.li@outlook.com>
2023-05-29 18:59:36 -0500

    Revert to the original version batch 80

--- scripts/KGMixtureOfExpertShen.sh
+++ scripts/KGMixtureOfExpertShen.sh
@@ -13,8 +13,8 @@ option="
     --output_dir ${OUTPUT_DIR}/KGMixtureOfExpertShen_Output
     --max_source_length 40
     --max_target_length 60 
-    --val_max_target_length 80
-    --test_max_target_length 80
+    --val_max_target_length 60
+    --test_max_target_length 60
     --num_train_epochs 30
     --learning_rate 3e-5 
     --fp16 
@@ -22,8 +22,8 @@ option="
     --do_eval 
     --do_predict 
     --eval_beams 3 
-    --per_device_train_batch_size 60
-    --per_device_eval_batch_size 60
+    --per_device_train_batch_size 80
+    --per_device_eval_batch_size 80
     --metric_for_best_model distinct_2
     --predict_with_generate 
     --load_best_model_at_end 
