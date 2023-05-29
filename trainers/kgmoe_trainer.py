3394277cf34a74f6223db9ff324014418b20f8e3
Yerong <yerong.li@outlook.com>
2023-05-29 15:58:53 -0500

    Compute metric

--- trainers/kgmoe_trainer.py
+++ trainers/kgmoe_trainer.py
@@ -352,25 +352,23 @@ class KGMoESeq2SeqTrainer(Seq2SeqTrainer):
         logging.info(self.compute_metrics is not None)
 
 
-        def compute_metrics(eval_prediction):
-            # Extract the predictions and labels from the EvalPrediction object
-            logging.info('Entering compute_metrics')
-            predictions = eval_prediction.predictions
-            label_ids = eval_prediction.label_ids
+        # Extract the predictions and labels from the EvalPrediction object
+        logging.info('Entering compute_metrics')
+        predictions = eval_prediction.predictions
+        label_ids = eval_prediction.label_ids
 
-            # Perform your desired computations on the predictions and labels
-            # Here's an example of computing accuracy
-            predictions = torch.argmax(predictions, dim=1)
-            correct = (predictions == label_ids).sum().item()
-            accuracy = correct / len(label_ids)
+        # Perform your desired computations on the predictions and labels
+        # Here's an example of computing accuracy
+        predictions = torch.argmax(predictions, dim=1)
+        correct = (predictions == label_ids).sum().item()
+        accuracy = correct / len(label_ids)
 
-            # Return a dictionary containing the computed metrics
-            metrics = {
-                'accuracy': accuracy
-            }
-            return metrics
-
-        metrics = compute_metrics(EvalPrediction(predictions=preds, label_ids=label_ids))
+        # Return a dictionary containing the computed metrics
+        metrics = {
+            'accuracy': accuracy
+        }
+        logging.info('metrics')
+        logging.info(metrics)
 
         # if self.compute_metrics is not None and preds is not None and label_ids is not None:
         #     metrics = self.compute_metrics(EvalPrediction(predictions=preds, label_ids=label_ids))
