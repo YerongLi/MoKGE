0d49858a0c9c0abcbd4b4d70d8bba1e00011f459
Yerong <yerong.li@outlook.com>
2023-05-29 19:22:18 -0500

    Do training

--- scripts/ev-KGMixtureOfExpertCho.sh
+++ scripts/ev-KGMixtureOfExpertCho.sh
@@ -48,15 +48,16 @@ option="
     --max_target_length 60 
     --val_max_target_length 60
     --test_max_target_length 60
-    --num_train_epochs 30
+    --num_train_epochs 3
     --learning_rate 3e-5 
     --mixture_embedding
-    --fp16 
+    --fp16
+    --do_train
     --do_eval 
     --do_predict 
     --eval_beams 3 
-    --per_device_train_batch_size 70
-    --per_device_eval_batch_size 70
+    --per_device_train_batch_size 100
+    --per_device_eval_batch_size 100
     --metric_for_best_model distinct_2
     --predict_with_generate 
     --overwrite_output_dir 
